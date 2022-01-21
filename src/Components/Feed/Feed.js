dacbf3aa1258328f2ad2d53332306aa54de3997f
recktg095 <lucas.magaldi@hotmail.com>
2022-01-21 18:26:45 -0300

    Begining post layout

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Ttbox from "../Ttbox/Ttbox";
+import Post from "../Post/Post";
 
 import './Feed.css'
 
@@ -8,12 +9,15 @@ function Feed() {
     <div className="feed">
         <div className="feed--header">
             {/* Header  */}
-            <h1>Header</h1>
+            <h2>Home</h2>
         </div>
         {/*   TwitterBox  */}
 
         <Ttbox />
-
+        <Post userName="@89tentacao" displayName="Nogueira" image="https://assets.pinterest.com/ext/embed.html?id=710583647465885601" />
+        <Post userName="@Vivendo" displayName="Vivendo" image= "https://assets.pinterest.com/ext/embed.html?id=44121271341431924" />
+        <Post />
+        <Post />
     </div>
     )
 }
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,16 +1,47 @@
 import React from "react";
 
+import './Post.css'
 
 function Post({
     displayName,
     userName,
     verified,
     timestamp,
-    text
+    text,
+    image,
+    avatar 
 }) {
     return (
         <div className="post">
-
+            <div className="post__avatar">
+                <img src="" alt="" />
+            </div>
+            <div className="post__body">
+                <div className="post__header">
+                    <div className="post__headerText">
+                        <h3>{displayName}</h3>
+                        <h5>{userName}</h5>
+                    </div>
+                    <div className="post__headerDescription">
+                        <p>Hahahahahahah, S2</p>
+                    </div>
+                </div>
+                <iframe src={image} height="600" width="480" frameborder="0" scrolling="no"  ></iframe>
+                <div className="post__footer">
+                    <a>
+                     <img className="post__footer-img" src="https://img.icons8.com/external-dreamstale-lineal-dreamstale/32/000000/external-chat-ui-dreamstale-lineal-dreamstale.png"/>
+                    </a>
+                    <a>
+                    <img className="post__footer-img" src="https://img.icons8.com/ios/50/000000/retweet.png"/>
+                    </a>
+                    <a>
+                    <img className="post__footer-img" src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/000000/external-heart-casino-kiranshastry-lineal-kiranshastry.png"/>
+                    </a>
+                    <a>
+                    <img className="post__footer-img" src="https://img.icons8.com/material-sharp/24/000000/share-3.png"/>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
