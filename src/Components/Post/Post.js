098948e0eb10b20a33ce95e3cda19a81ab3e6123
recktg095 <lucas.magaldi@hotmail.com>
2022-01-21 10:28:27 -0300

    Post folder structure and files initial config

---
+++ src/Components/Post/Post.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Post({
+    displayName,
+    userName,
+    verified,
+    timestamp,
+    text
+}) {
+    return (
+        <div className="post">
+
+        </div>
+    )
+}
+
+
+export default Post;
